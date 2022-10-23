ea82a88cd48855d782a6bab2ac6b695ac0769ea5
Marian13 <mariankostyk13895@gmail.com>
2022-10-23 19:53:04 +0300

    chore(links): update edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/marian13/convenient_service_docs/blob/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/marian13/convenient_service_docs/blob/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
