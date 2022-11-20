9110f0b6bccbe8b510f341071441f20152b9b0c0
Marian13 <mariankostyk13895@gmail.com>
2022-11-20 18:22:14 +0200

    refactor(categories): introduce api

--- sidebars.js
+++ sidebars.js
@@ -62,7 +62,7 @@ const sidebars = {
     },
     {
       type: 'category',
-      label: 'Plugins',
+      label: 'API',
       link: {
         type: 'generated-index',
       },
@@ -70,116 +70,116 @@ const sidebars = {
       items: [
         {
           type: 'category',
-          label: 'Common',
+          label: 'Plugins',
           link: {
             type: 'generated-index',
           },
           collapsed: true,
           items: [
-            'plugins/common/overview',
             {
               type: 'category',
-              label: 'Assigns attributes in constructor',
+              label: 'Common',
               link: {
                 type: 'generated-index',
               },
               collapsed: true,
               items: [
-                'plugins/common/assigns_attributes_in_constructor/using_active_model_attribute_assignment',
-                'plugins/common/assigns_attributes_in_constructor/using_dry_initializer',
+                'api/plugins/common/overview',
+                {
+                  type: 'category',
+                  label: 'Assigns attributes in constructor',
+                  link: {
+                    type: 'generated-index',
+                  },
+                  collapsed: true,
+                  items: [
+                    'api/plugins/common/assigns_attributes_in_constructor/using_active_model_attribute_assignment',
+                    'api/plugins/common/assigns_attributes_in_constructor/using_dry_initializer',
+                  ],
+                },
+                'api/plugins/common/caches_constructor_params',
+                'api/plugins/common/caches_return_value',
+                'api/plugins/common/can_be_copied',
+                'api/plugins/common/has_around_callbacks',
+                'api/plugins/common/has_attributes',
+                'api/plugins/common/has_callbacks',
+                'api/plugins/common/has_constructor',
+                'api/plugins/common/has_internals',
+                'api/plugins/common/normalizes_env',
               ],
             },
-            'plugins/common/caches_constructor_params',
-            'plugins/common/caches_return_value',
-            'plugins/common/can_be_copied',
-            'plugins/common/has_around_callbacks',
-            'plugins/common/has_attributes',
-            'plugins/common/has_callbacks',
-            'plugins/common/has_constructor',
-            'plugins/common/has_internals',
-            'plugins/common/normalizes_env',
+            {
+              type: 'category',
+              label: 'Service',
+              link: {
+                type: 'generated-index',
+              },
+              collapsed: true,
+              items: [
+                'api/plugins/service/overview',
+                'api/plugins/service/can_recalculate_result',
+                'api/plugins/service/has_result',
+                'api/plugins/service/has_result_method_steps',
+                'api/plugins/service/has_result_params_validations',
+                'api/plugins/service/has_result_short_syntax',
+                'api/plugins/service/has_result_steps',
+                'api/plugins/service/raises_on_double_result',
+                'api/plugins/service/wraps_result_in_db_transaction',
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Result',
+              link: {
+                type: 'generated-index',
+              },
+              collapsed: true,
+              items: [
+                'api/plugins/result/overview',
+                'api/plugins/result/can_recalculate_result',
+                'api/plugins/result/has_result_short_syntax',
+                'api/plugins/result/marks_result_status_as_checked',
+                'api/plugins/result/raises_on_not_checked_result_status',
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Step',
+              link: {
+                type: 'generated-index',
+              },
+              collapsed: true,
+              items: [
+                'api/plugins/step/overview',
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Step',
+              link: {
+                type: 'generated-index',
+              },
+              collapsed: true,
+              items: [
+                'api/plugins/internals/overview',
+              ],
+            },
           ],
         },
         {
           type: 'category',
-          label: 'Service',
+          label: 'Configs',
           link: {
             type: 'generated-index',
           },
           collapsed: true,
           items: [
-            'plugins/service/overview',
-            'plugins/service/can_recalculate_result',
-            'plugins/service/has_result',
-            'plugins/service/has_result_method_steps',
-            'plugins/service/has_result_params_validations',
-            'plugins/service/has_result_short_syntax',
-            'plugins/service/has_result_steps',
-            'plugins/service/raises_on_double_result',
-            'plugins/service/wraps_result_in_db_transaction',
+            'api/configs/standard',
           ],
         },
         {
           type: 'category',
-          label: 'Result',
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: true,
-          items: [
-            'plugins/result/overview',
-            'plugins/result/can_recalculate_result',
-            'plugins/result/has_result_short_syntax',
-            'plugins/result/marks_result_status_as_checked',
-            'plugins/result/raises_on_not_checked_result_status',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Step',
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: true,
-          items: [
-            'plugins/step/overview',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Step',
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: true,
-          items: [
-            'plugins/internals/overview',
-          ],
-        },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Configs',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: true,
-      items: [
-        'configs/standard',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Tests',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: true,
-      items: [
-        {
-          type: 'category',
-          label: 'RSpec',
+          label: 'Tests',
           link: {
             type: 'generated-index',
           },
@@ -187,13 +187,23 @@ const sidebars = {
           items: [
             {
               type: 'category',
-              label: 'Helpers',
+              label: 'RSpec',
               link: {
                 type: 'generated-index',
               },
               collapsed: true,
               items: [
-                'tests/rspec/helpers/wrap_method',
+                {
+                  type: 'category',
+                  label: 'Helpers',
+                  link: {
+                    type: 'generated-index',
+                  },
+                  collapsed: true,
+                  items: [
+                    'api/tests/rspec/helpers/wrap_method',
+                  ],
+                },
               ],
             },
           ],
