36659c1dacc7477569e60b1ed2e8a8d5b983b6f2
Marian13 <mariankostyk13895@gmail.com>
2022-12-19 23:38:31 +0200

    refactor(version): extract version into constant

---
+++ src/constants/version.js
@@ -0,0 +1,8 @@
+
+/**
+ * Convenient Service version.
+ * - https://github.com/marian13/convenient_service/blob/main/lib/convenient_service/version.rb
+ */
+const version = '0.3.1';
+
+export default version;
