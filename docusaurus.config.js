1b56e1f28b7abac93a58d6a204d49b7008236c43
Marian13 <mariankostyk13895@gmail.com>
2024-02-26 01:01:37 +0200

    chore(search): use lunr instead of algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,54 +121,67 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-      /**
-       * NOTE: Algolia configuration template was copied from:
-       * - https://docusaurus.io/docs/search#using-algolia-docsearch
-       *
-       * NOTE: Credentials are taken from the Algolia registration email.
-       *
-       * NOTE: More Algolia configuration options can be found here:
-       * - https://docsearch.algolia.com/docs/templates/#docusaurus-v2-template
-       *
-       * NOTE: Docusaurus classic preset already contains Algolia. That is why NO new npm package is installed.
-       */
-      algolia: {
-        /**
-         * The application ID provided by Algolia.
-         */
-        appId: '28FNFYWD0P',
-
-        /**
-         * Public API key: it is safe to commit it.
-         * - https://docsearch.algolia.com/docs/legacy/faq/#can-i-share-the-apikey-in-my-repo
-         * - https://dashboard.algolia.com/account/api-keys/all?applicationId=28FNFYWD0P
-         * - https://www.algolia.com/doc/guides/security/api-keys/#create-and-manage-your-api-keys
-         */
-        apiKey: '42110676756fcfbcdf9d0f0fef8cb56d',
-
-        /**
-         * Index name. Can be found in the Algolia Dashboard.
-         * - https://dashboard.algolia.com/apps/28FNFYWD0P/explorer/browse/convenient_service?searchMode=search
-         */
-        indexName: 'convenient_service',
-
-        /**
-         * https://docusaurus.io/docs/search#contextual-search
-         */
-        contextualSearch: true,
-
-        /**
-         * Path for search page that enabled by default (`false` to disable it).
-         */
-        searchPagePath: 'search',
-
-        /**
-         * - https://docsearch.algolia.com/docs/crawler/#how-often-will-you-crawl-my-website
-         * - https://www.algolia.com/doc/tools/crawler/apis/configuration/schedule/
-         */
-        schedule: 'every 1 day at 3:00 pm',
-      },
+      // /**
+      //  * NOTE: Stopped to use Algolia since there is NO way to trigger reindexing on a free plan.
+      //  *
+      //  * NOTE: Algolia configuration template was copied from:
+      //  * - https://docusaurus.io/docs/search#using-algolia-docsearch
+      //  *
+      //  * NOTE: Credentials are taken from the Algolia registration email.
+      //  *
+      //  * NOTE: More Algolia configuration options can be found here:
+      //  * - https://docsearch.algolia.com/docs/templates/#docusaurus-v2-template
+      //  *
+      //  * NOTE: Docusaurus classic preset already contains Algolia. That is why NO new npm package is installed.
+      //  */
+      // algolia: {
+      //   /**
+      //    * The application ID provided by Algolia.
+      //    */
+      //   appId: '28FNFYWD0P',
+      //
+      //   /**
+      //    * Public API key: it is safe to commit it.
+      //    * - https://docsearch.algolia.com/docs/legacy/faq/#can-i-share-the-apikey-in-my-repo
+      //    * - https://dashboard.algolia.com/account/api-keys/all?applicationId=28FNFYWD0P
+      //    * - https://www.algolia.com/doc/guides/security/api-keys/#create-and-manage-your-api-keys
+      //    */
+      //   apiKey: '42110676756fcfbcdf9d0f0fef8cb56d',
+      //
+      //   /**
+      //    * Index name. Can be found in the Algolia Dashboard.
+      //    * - https://dashboard.algolia.com/apps/28FNFYWD0P/explorer/browse/convenient_service?searchMode=search
+      //    */
+      //   indexName: 'convenient_service',
+      //
+      //   /**
+      //    * https://docusaurus.io/docs/search#contextual-search
+      //    */
+      //   contextualSearch: true,
+      //
+      //   /**
+      //    * Path for search page that enabled by default (`false` to disable it).
+      //    */
+      //   searchPagePath: 'search',
+      //
+      //   /**
+      //    * - https://docsearch.algolia.com/docs/crawler/#how-often-will-you-crawl-my-website
+      //    * - https://www.algolia.com/doc/tools/crawler/apis/configuration/schedule/
+      //    */
+      //   schedule: 'every 1 day at 3:00 pm',
+      // },
     }),
+    plugins: [
+      [
+        /**
+         * - https://github.com/praveenn77/docusaurus-lunr-search?tab=readme-ov-file#using-an-option-eg-languages-in-the-plugin
+         */
+        require.resolve('docusaurus-lunr-search'),
+        {
+          languages: ['en']
+        },
+      ],
+    ],
 };
 
 module.exports = config;
