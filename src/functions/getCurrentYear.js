fd46c089aec2442a3b3ecd333a1ea47fe6974555
Marian13 <mariankostyk13895@gmail.com>
2024-03-20 02:06:07 +0200

    chore(functions): introduce getCurrentYear

---
+++ src/functions/getCurrentYear.js
@@ -0,0 +1,6 @@
+/**
+ * - https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript
+ */
+const getCurrentYear = () => new Date().getFullYear();
+
+export default getCurrentYear;
