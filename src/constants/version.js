f7ff14c8ad4a8e017f99e061702a6fab988c9ef9
Marian13 <mariankostyk13895@gmail.com>
2024-08-15 19:48:02 +0300

    docs(version): update version

--- src/constants/version.js
+++ src/constants/version.js
@@ -3,6 +3,6 @@
  * Convenient Service version.
  * - https://github.com/marian13/convenient_service/blob/main/lib/convenient_service/version.rb
  */
-const version = '0.19.0';
+const version = '0.19.1';
 
 export default version;
