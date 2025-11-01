528a136c8c016294ec645dd5c58853598dfbcd14
Marian13 <mariankostyk13895@gmail.com>
2025-11-01 18:48:35 +0200

    chore(docs): add logo

--- fumadocs/convenient_service_docs/lib/layout.shared.tsx
+++ fumadocs/convenient_service_docs/lib/layout.shared.tsx
@@ -3,7 +3,23 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
-    },
+      /**
+       * NOTE: Copied from Fumadocs own docs.
+       * - https://github.com/fuma-nama/fumadocs/blob/646dd3ba78c8d27f94ef9d907ec4871dd8c1a19b/apps/docs/app/docs/layout.tsx#L20
+       */
+      title: (
+        <>
+          <img
+            src="/logo.png"
+            alt="Convenient Service Logo"
+            className="h-8 w-auto"
+          />
+
+          <span className="font-medium [.uwu_&]:hidden max-md:hidden">
+            Convenient Service
+          </span>
+        </>
+      ),
+    }
   };
 }
