4944fa8a73e1eef19f463a5fe52de9212a54ec69
Marian13 <mariankostyk13895@gmail.com>
2025-11-01 17:55:46 +0200

    chore(docs): ignore outer yarn.lock

--- fumadocs/convenient_service_docs/next.config.mjs
+++ fumadocs/convenient_service_docs/next.config.mjs
@@ -1,10 +1,25 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  turbopack: {
+    /**
+     * NOTE:
+     *   Next.js also looks for yarn.lock in the parent folders.
+     *   The config below disables that behaviour.
+     *   Otherwise, `yarn build` has warnings.
+     */
+    root: __dirname
+  }
 };
 
 export default withMDX(config);
