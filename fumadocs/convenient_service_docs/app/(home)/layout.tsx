219e0ebe722e3a3988c02aae4b5b6d40a9587845
Marian13 <mariankostyk13895@gmail.com>
2025-11-01 19:04:48 +0200

    chore(docs): add home page title

--- fumadocs/convenient_service_docs/app/(home)/layout.tsx
+++ fumadocs/convenient_service_docs/app/(home)/layout.tsx
@@ -1,6 +1,18 @@
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
+/**
+ * NOTE: Adds `<title>Convenient Service</title>` and `<meta name="description" content="Convenient Service User Docs">`.
+ * - https://nextjs.org/learn/dashboard-app/adding-metadata#page-title-and-descriptions
+ * - https://github.com/fuma-nama/fumadocs/blob/646dd3ba78c8d27f94ef9d907ec4871dd8c1a19b/apps/docs/app/(home)/showcase/page.tsx#L20
+ */
+export const metadata = {
+  title: {
+    default: "Convenient Service"
+  },
+  description: "Convenient Service User Docs",
+};
+
 export default function Layout({ children }: LayoutProps<'/'>) {
   return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
 }
