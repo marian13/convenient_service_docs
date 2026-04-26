13a64fe12113cddeafd94ec1bb69606e619a45cd
Marian13 <mariankostyk13895@gmail.com>
2026-04-26 02:05:02 +0200

    chore(convenient_service_comprehensive_user_docs): add next iteration

--- lib/builder.rb
+++ lib/builder.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'ferrum'
+require 'concurrent'
+require 'logger'
 require 'retriable'
 require 'rexml/document'
 require 'fileutils'
@@ -8,100 +10,171 @@ require 'socket'
 require 'net/http'
 require 'uri'
 
-def root
-  @root ||= File.expand_path('..', __dir__)
-end
+class Builder
+  def run
+    remove_folder(dist)
 
-def port
-  @port ||= begin
-    server = TCPServer.new('127.0.0.1', 0)
+    server_pid = spawn("PORT=#{port} bundle exec ruby lib/dev_server.rb", chdir: root)
 
-    port = server.addr[1]
+    wait_server_healthcheck
 
-    server.close
+    build_urls
 
-    port
+    touch_folder(dist('src'))
+    copy_folder(src('docs'), dist('docs'))
+    copy_folder(src('components'), dist('src/components'))
+    copy_folder(src('global'), dist('src/global'))
+    copy_folder(src('utils'), dist('src/utils'))
+    copy_folder(src('public'), dist('public'))
+
+    copy_partial_styles
+
+    copy_file(src('sitemap.xml'), dist('sitemap.xml'))
+
+    kill_process(server_pid)
+
+    logger.info('done')
+  end
+
+  private
+
+  def root
+    @root ||= File.expand_path('..', __dir__)
+  end
+
+  def port
+    @port ||= begin
+      server = TCPServer.new('127.0.0.1', 0)
+      port = server.addr[1]
+      server.close
+      port
+    end
+  end
+
+  def pool_size
+    @pool_size ||= 8
+  end
+
+  def locs
+    @locs ||= if ARGV.any?
+      ARGV
+    else
+      doc = REXML::Document.new(File.read(src('sitemap.xml')))
+      doc.elements.collect('urlset/url/loc', &:text)
+    end
+  end
+
+  def urls
+    @urls ||= locs.map do |loc|
+      uri = URI.parse(loc)
+      uri.scheme = 'http'
+      uri.host = 'localhost'
+      uri.port = port
+      uri
+    end
+  end
+
+  def dynamic_urls
+    @dynamic_urls ||= urls.reject { |url| url.path.end_with?('.md') }
+  end
+
+  def logger
+    @logger ||= Logger.new($stdout)
+  end
+
+  def build_urls
+    pool = Concurrent::FixedThreadPool.new(pool_size)
+
+    dynamic_urls.each { |url| Concurrent::Future.execute(executor: pool) { build_url(url) } }
+
+    pool.shutdown
+    pool.wait_for_termination
+  end
+
+  def copy_partial_styles
+    Dir.glob(src('views/partials/**/*.css')).each do |css_file|
+      dest = css_file.sub(src, dist)
+      touch_folder(File.dirname(dest))
+      copy_file(css_file, dest)
+    end
+  end
+
+  def wait_server_healthcheck
+    wait { Net::HTTP.get(URI("http://localhost:#{port}/healthcheck")) }
+  end
+
+  def kill_process(pid)
+    Process.kill('TERM', pid)
+    Process.wait(pid)
+  end
+
+  def src(path = '')
+    File.join(root, 'src', path)
+  end
+
+  def dist(path = '')
+    File.join(root, 'dist', path)
+  end
+
+  def build_url(url)
+    save_url(url, fetch_url(url))
+  end
+
+  def fetch_url(url)
+    browser = Ferrum::Browser.new(timeout: 15, headless: true)
+
+    browser.page.command("Page.addScriptToEvaluateOnNewDocument", source: "window.__build__ = true")
+    browser.goto(url.to_s)
+    browser.network.wait_for_idle
+
+    wait(seconds: 5) { browser.evaluate("window.__ready__") }
+
+    content = <<~HTML
+      <!DOCTYPE html>
+      #{browser.evaluate('document.documentElement.outerHTML')}
+    HTML
+
+    browser.quit
+
+    content
+  end
+
+  def save_url(url, content)
+    dist_path =
+      if url.path == '/'
+        dist('index.html')
+      elsif url.path.end_with?('.html')
+        dist(url.path)
+      else
+        dist(File.join(url.path.chomp('/'), 'index.html'))
+      end
+
+    touch_folder(File.dirname(dist_path))
+
+    File.write(dist_path, content)
+
+    logger.info("built #{dist_path.delete_prefix("#{root}/")}")
+  end
+
+  def touch_folder(path)
+    FileUtils.mkdir_p(path)
+  end
+
+  def copy_file(src, dest)
+    FileUtils.cp(src, dest)
+  end
+
+  def copy_folder(src, dest)
+    FileUtils.cp_r(src, dest, remove_destination: true)
+  end
+
+  def remove_folder(path)
+    FileUtils.rm_rf(path)
+  end
+
+  def wait(seconds: 2, &block)
+    Retriable.retriable(tries: (seconds / 0.1).ceil, base_interval: 0.1) { raise unless block.call }
   end
 end
 
-def locs
-  @locs ||= if ARGV.any?
-    ARGV
-  else
-    doc = REXML::Document.new(File.read(File.join(root, 'sitemap.xml')))
-
-    doc.elements.collect('urlset/url/loc', &:text)
-  end
-end
-
-def urls
-  @urls ||= locs.map do |loc|
-    uri = URI.parse(loc)
-    uri.scheme = 'http'
-    uri.host = 'localhost'
-    uri.port = port
-    uri
-  end
-end
-
-def dynamic_urls
-  @dynamic_urls ||= urls.reject { |url| url.path.end_with?('.md') }
-end
-
-def wait(tries: 20, &block)
-  Retriable.retriable(tries: tries, base_interval: 0.1) { raise unless block.call }
-end
-
-def resolve_dist_path(url)
-  if url.path == '/'
-    File.join(root, 'dist/index.html')
-  elsif url.path.end_with?('.html')
-    File.join(root, 'dist', url.path)
-  else
-    File.join(root, 'dist', url.path.chomp('/'), 'index.html')
-  end
-end
-
-def build_url(url)
-  browser = Ferrum::Browser.new(timeout: 15, headless: true)
-
-  browser.page.command("Page.addScriptToEvaluateOnNewDocument", source: "window.__build__ = true")
-  browser.goto(url.to_s)
-  browser.network.wait_for_idle
-
-  wait(tries: 50) { browser.evaluate("window.__ready__") }
-
-  content = "<!DOCTYPE html>\n#{browser.evaluate('document.documentElement.outerHTML')}"
-
-  browser.quit
-
-  dist_path = resolve_dist_path(url)
-
-  FileUtils.mkdir_p(File.dirname(dist_path))
-  File.write(dist_path, content)
-
-  puts "built #{dist_path.delete_prefix("#{root}/")}"
-end
-
-FileUtils.rm_rf(File.join(root, 'dist'))
-
-server_pid = spawn("PORT=#{port} bundle exec ruby lib/dev_server.rb", chdir: root)
-
-wait { Net::HTTP.get(URI("http://localhost:#{port}/healthcheck")) }
-
-dynamic_urls.map { |url| Thread.new { build_url(url) } }.each(&:join)
-
-FileUtils.cp_r(File.join(root, 'src/docs'), File.join(root, 'dist/docs'), remove_destination: true)
-FileUtils.mkdir_p(File.join(root, 'dist/src'))
-FileUtils.cp_r(File.join(root, 'src/components'), File.join(root, 'dist/src/components'), remove_destination: true)
-FileUtils.cp_r(File.join(root, 'src/global'), File.join(root, 'dist/src/global'), remove_destination: true)
-FileUtils.cp_r(File.join(root, 'src/utils'), File.join(root, 'dist/src/utils'), remove_destination: true)
-FileUtils.cp_r(File.join(root, 'src/public'), File.join(root, 'dist/public'), remove_destination: true)
-FileUtils.mkdir_p(File.join(root, 'dist/views'))
-FileUtils.cp_r(File.join(root, 'src/views/partials'), File.join(root, 'dist/views/partials'), remove_destination: true)
-FileUtils.cp(File.join(root, 'sitemap.xml'), File.join(root, 'dist/sitemap.xml'))
-
-Process.kill('TERM', server_pid)
-Process.wait(server_pid)
-
-puts 'done'
+Builder.new.run
