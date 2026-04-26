2546e705aad908110fe5b557cdc1b744b73f7763
Marian13 <mariankostyk13895@gmail.com>
2026-04-26 02:25:05 +0200

    chore(convenient_service_comprehensive_user_docs): add next iteration

--- lib/dev_server.rb
+++ lib/dev_server.rb
@@ -1,81 +1,81 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 
-def root
-  @root ||= File.expand_path('..', __dir__)
-end
+class DevServer < Sinatra::Base
+  set :port, ENV.fetch('PORT', 8100).to_i
+  set :views, File.join(File.expand_path('..', __dir__), 'src/views')
+  set :erb, layout: :"layout.html"
 
-def port
-  @port ||= ENV.fetch('PORT', 8100).to_i
-end
+  disable :static
 
-set :port, port
-set :views, File.join(root, 'src/views')
-set :erb, layout: :"layout.html"
+  helpers do
+    def root
+      @root ||= File.expand_path('..', __dir__)
+    end
 
-disable :static
+    def path
+      request.path_info
+    end
 
-helpers do
-  def path
-    request.path_info
+    def src_file(url_path)
+      File.join(root, 'src', url_path.sub(%r{^/src}, ''))
+    end
+
+    def send_static_file(url_path)
+      send_file src_file(url_path)
+    end
+
+    def send_dynamic_page(url_path)
+      @content = File.read(src_file(url_path))
+
+      erb :"page.html"
+    end
+
+    def send_dynamic_doc(url_path)
+      @path = url_path
+
+      erb :"doc.html"
+    end
+
+    def render_partial(name)
+      erb :"partials/#{name}/index.html", layout: false
+    end
   end
 
-  def src_file(url_path)
-    File.join(root, 'src', url_path.sub(%r{^/src}, ''))
+  get '/healthcheck' do
+    'ok'
   end
 
-  def send_static_file(url_path)
-    send_file src_file(url_path)
+  get %r{/(src|public)/(.+\.(js|css|svg|png))} do
+    send_static_file path
   end
 
-  def send_dynamic_page(url_path)
-    @content = File.read(src_file(url_path))
-
-    erb :"page.html"
+  get %r{/views/partials/(.+\.css)} do
+    send_static_file path
   end
 
-  def send_dynamic_doc(url_path)
-    @path = url_path
-
-    erb :"doc.html"
+  get '/' do
+    send_dynamic_page '/pages/index.html'
   end
 
-  def render_partial(name)
-    erb :"partials/#{name}/index.html", layout: false
+  get %r{/docs/(.+\.md)} do
+    content_type 'text/markdown'
+
+    send_static_file path
   end
-end
 
-get '/healthcheck' do
-  'ok'
-end
+  get %r{/docs/(.+\.html)} do
+    send_dynamic_doc path.sub(/\.html$/, '.md')
+  end
 
-get %r{/(src|public)/(.+\.(js|css|svg|png))} do
-  send_static_file path
-end
+  get %r{/pages/(.+\.html)} do
+    send_dynamic_page path
+  end
 
-get %r{/views/partials/(.+\.css)} do
-  send_static_file path
-end
+  not_found do
+    send_static_file '/public/404.html'
+  end
 
-get '/' do
-  send_dynamic_page '/pages/index.html'
-end
-
-get %r{/docs/(.+\.md)} do
-  content_type 'text/markdown'
-
-  send_static_file path
-end
-
-get %r{/docs/(.+\.html)} do
-  send_dynamic_doc path.sub(/\.html$/, '.md')
-end
-
-get %r{/pages/(.+\.html)} do
-  send_dynamic_page path
-end
-
-not_found do
-  send_static_file '/public/404.html'
+  run! if app_file == $0
 end
