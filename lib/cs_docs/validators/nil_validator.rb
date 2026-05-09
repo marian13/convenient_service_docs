de112360b374dc7a73417d88bc6fb046d0219dfc
Marian13 <mariankostyk13895@gmail.com>
2026-05-09 23:15:00 +0200

    chore(convenient_service_comprehensive_user_docs): add next iteration

--- lib/cs_docs/validators/nil_validator.rb
+++ lib/cs_docs/validators/nil_validator.rb
@@ -2,10 +2,8 @@
 
 require "active_model"
 
-module CSDocs
-  class NilValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      record.errors.add(attribute, :nil, message: "can't be nil") if value.nil?
-    end
+class NilValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors.add(attribute, :nil, message: "can't be nil") if value.nil?
   end
 end
